e374016d2a18d2e7251bbf37b4d8e372757bbb5a
3NSN <tsimofeiyushkis@gmail.com>
2020-01-31 02:16:17 +0300

    Version 1.1

--- solution.py
+++ solution.py
@@ -1,26 +1,24 @@
 month_name = str(input('Enter month name: '))
-month_date = int(input('Enter month date: '))
+month_date = int(input('Enter month date: '))#
+
 months31 = ['Janury', 'March', 'May', 'Jule', 'August', 'October', 'December']
 months30 = ['June', 'September', 'November', 'April']
 months29 = ['February']
 
-correction = 1
-
-if month_date < 0 or month_date > 31:
-    print ("Таких месяцев или дат не существует")
-    exit(0)
+beginning_of_month = 1
 
 if month_name in months31:
-    date_range = range(1, 31 + correction)
+    end_of_month = 31
 elif month_name in months30:
-    date_range = range(1, 30 + correction)
+    end_of_month = 30
 elif month_name in months29:
-    date_range = range(1, 29 + correction)
-else:
+    end_of_month = 29
+
+if month_date < 0 or month_date > 31 or month_name not in (months31 + months30 + months29):
     print("Таких месяцев или дат не существует")
     exit(0)
 
-if month_date in date_range:
+if beginning_of_month <= month_date <= end_of_month:
     print("Да, " + month_name + " содержит число " + str(month_date))
 else:
-    print("Нет, " + month_name + " содержит только числа с " + str(date_range.start) + " по " + str(date_range.stop - correction))
+    print("Нет, " + month_name + " содержит только числа с " + str(beginning_of_month) + " по " + str(end_of_month))
